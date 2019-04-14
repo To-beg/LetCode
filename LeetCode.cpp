2cf67e2ab84ad7f9f66fefdfcbb7c24717bd31c5
To-beg <42836931+To-beg@users.noreply.github.com>
2019-04-14 17:45:54 +0800

    LeetCode.cpp
    The source file of the answer.

---
+++ LeetCode.cpp
@@ -0,0 +1,14 @@
+#include "pch.h"
+#include"function.h"
+#include"LeetCode.h"
+#include <iostream>
+#include<vector>
+#include<map>
+
+
+int main()
+{
+	string s = "babad";
+	Solution Result;
+	Result.longestPalindrome(s);
+}
